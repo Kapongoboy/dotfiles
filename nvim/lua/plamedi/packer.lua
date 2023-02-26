06448c82fbb0a42b9a977334817de0ba0db63569
Plamedi Luis <plamedi.luis11@gmail.com>
2023-02-26 10:47:27 +0200

    packer installer added

--- nvim/lua/plamedi/packer.lua
+++ nvim/lua/plamedi/packer.lua
@@ -1,8 +1,20 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
+local ensure_packer = function ()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') ..
+    '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+      'git', 'clone', '--depth', '1',
+      'https://github.com/wbthomason/packer.nvim', install_path
+    })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
+local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
