d4dfe85931714dd7b6e734734c3807f43a11cb61
Plamedi Luis <plamedi.luis11@gmail.com>
2023-10-13 01:31:58 +0200

    added colibri color theme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,8 +1,10 @@
 function ColorMyPencils(color)
 	-- color = color or "kanagawa-wave"
 	-- color = color or "catppuccin"
+	color = color or "colibri"
     -- color = color or "nord"
-    color = color or "darcula"
+    -- color = color or "darcula"
+    -- color = color or "shades_of_purple"
     -- color = color or "darcula-solid"
 	-- color = color or "gruvbox"
 	-- color = color or "gruvbox-material"
@@ -11,7 +13,8 @@ function ColorMyPencils(color)
 	-- color = color or "yami"
 	-- color = color or "catppuccin-latte"
     -- color = color or "tokyonight"
-	-- vim.cmd.colorscheme(color)
+
+	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
