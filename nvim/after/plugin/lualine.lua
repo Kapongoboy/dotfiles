11de9f5cb9097e4ebdf94f34a9c27a6f26a3db10
Plamedi Luis <plamedi.luis11@gmail.com>
2023-10-19 19:01:32 +0200

    switching up the lualine look

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,9 +1,9 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'auto',
+    theme = 'powerline',
     component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},
