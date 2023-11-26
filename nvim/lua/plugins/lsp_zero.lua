0b6edd9d34b5cf727704e8cca545ebc069cf9ee2
Plamedi Luis <plamedi.luis11@gmail.com>
2023-11-26 18:56:10 +0200

    added more lsp and treesitter stuff

--- nvim/lua/plugins/lsp_zero.lua
+++ nvim/lua/plugins/lsp_zero.lua
@@ -8,5 +8,9 @@ return {
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
     {'L3MON4D3/LuaSnip'},
+    {'rafamadriz/friendly-snippets'},
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,11 @@
-local M = {
-    "nvim-treesitter/nvim-treesitter",
+return  {
+  {"nvim-treesitter/nvim-treesitter",
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
+    end},
+    {"nvim-treesitter/nvim-treesitter-context"},
+    {"nvim-treesitter/nvim-treesitter-textobjects",
+  after = "nvim-treesitter",},
+  {'nvim-treesitter/nvim-treesitter-refactor'},
 }
 
-return { M }
