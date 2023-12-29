1967713e2c4c96aa7bd2fae0c0cdea4beb0eaa24
Plamedi Luis <plamedi.luis@gmail.com>
2023-12-29 12:47:51 +0200

    couple of fish sweets

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,10 @@
 if status is-interactive
+    function nf
+        set file (fzf)
+        if test -n "$file"
+            nvim "$file"
+        end
+    end
 
     # User specific environment
     if not contains -- "$HOME/.local/bin" $PATH
@@ -8,14 +14,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     abbr -a re cd ~/Documents/repos
     abbr -a q exit
+    abbr -a pls sudo pacman -S --needed
+    abbr -a pvs source ~/Documents/repos/pocket/yolov7/venv/bin/activate.fish
     fish_vi_key_bindings
 
     # environment variables
     set -Ux VOLTA_HOME $HOME/.volta
     set -x PATH $VOLTA_HOME/bin $PATH
+    set -x PATH $HOME/.zig $PATH
+    set -x PATH $HOME/.code/bin $PATH
+    set PATH $HOME/.cargo/bin $PATH
 
     starship init fish | source
-
-    set PATH $HOME/.cargo/bin $PATH
 
 end
