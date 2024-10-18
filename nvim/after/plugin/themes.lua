b4b28f3031ec3913fca48b9cb966a9759b7cc565
Plamedi on EoS <plamedi.luis11@gmail.com>
2024-10-18 15:12:09 +0200

    slim girl summer

--- nvim/after/plugin/themes.lua
+++ nvim/after/plugin/themes.lua
@@ -1,4 +1,4 @@
--- vim.opt.background = "dark"
-vim.cmd.colorscheme("gruvbox-material")
+vim.cmd.colorscheme("nord")
+vim.cmd.colorscheme("gruvbuddy")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
