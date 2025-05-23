4e963095f9afbbd562c30204d085e8d69c875616
Plamedi Luis <plamedi.luis11@gmail.com>
2025-05-23 10:42:25 +0200

    color change and font switch for wez

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,16 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "GruvboxDarkHard"
+config.color_scheme = "Rebecca (base16)"
 config.font = wezterm.font_with_fallback({
+	{
+		family = "FiraMono Nerd Font",
+    weight = "Medium",
+		harfbuzz_features = { "calt", "clig", "liga" },
+	},
+	{
+		family = "Hack Nerd Font Mono",
+	},
 	{
 		family = "JetBrains Mono",
 		weight = "DemiBold",
@@ -14,25 +22,17 @@ config.font = wezterm.font_with_fallback({
 		weight = "DemiBold",
 	},
 	{
-		family = "Hasklug Nerd Font Mono",
-		weight = "DemiBold",
-	},
-	{
 		family = "Cascadia Code PL",
 		weight = "DemiBold",
 		harfbuzz_features = { "calt", "ss01", "ss20" },
 	},
-	{
-		family = "Hack Nerd Font Mono",
-		weight = "Regular",
-	},
 })
 config.font_size = 11
 config.dpi = 96.0
-config.animation_fps = 60
+config.animation_fps = 120
 config.enable_wayland = true
 
-config.window_background_opacity = 1.0
+config.window_background_opacity = 0.85
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.bold_brightens_ansi_colors = true
