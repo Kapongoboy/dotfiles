8cb3b82993447a12050fe61dc6c77b984d4f60d0
Plamedi Luis <plamluis@proton.me>
2025-12-22 06:26:59 +0200

    chore(nvim): bump telescope and other packages versions

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
-
 -- plugins/telescope.lua:
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+	"nvim-telescope/telescope.nvim",
+	tag = "v0.2.0",
+	dependencies = { "nvim-lua/plenary.nvim" },
+}
