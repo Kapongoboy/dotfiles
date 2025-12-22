4a1936c4adcc550fb5acc42267f68d751cb1e573
Plamedi Luis <plamluis@proton.me>
2025-12-22 06:26:30 +0200

    refactor(nvim): update to current breaking changes state of treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,7 @@
-return  {
-  {"nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end},
-    {"nvim-treesitter/nvim-treesitter-context"},
-    {"nvim-treesitter/nvim-treesitter-textobjects",
-  after = "nvim-treesitter",},
-  {'nvim-treesitter/nvim-treesitter-refactor'},
+return {
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	{ "nvim-treesitter/nvim-treesitter-context" },
+	{ "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter", branch = "main" },
 }
-
